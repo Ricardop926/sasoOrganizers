9d64ad6a00edc91e1bac07c3badb645d3c442468
unknown <ricardo.porras26@outlook.com>
2023-09-22 01:55:34 -0500

    se crean rutas y funciones para la base da datos

---
+++ appGlry/controllerGallety/book.controller.js
@@ -0,0 +1,21 @@
+async function getInicializationData(req, res){
+    console.log("getInicializationData");
+
+}
+
+async function getOneBook(req, res){
+    console.log("getInicializationData");
+
+}
+
+async function newBook(req, res){
+    console.log("getInicializationData");
+
+}
+
+module.exports = {
+    getInicializationData, 
+    getOneBook,
+    newBook
+
+}
